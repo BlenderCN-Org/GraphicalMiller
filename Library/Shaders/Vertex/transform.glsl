59acabb386e462809d7b50b1250df2be53404570
Jeremy Miller <jeremymm@users.noreply.github.com>
2017-02-20 23:54:11 -0500

    remove commented out code

--- Library/Shaders/Vertex/transform.glsl
+++ Library/Shaders/Vertex/transform.glsl
@@ -3,7 +3,5 @@ attribute vec4 gl_Vertex;
 
 void main()
 {
-  // gl_Position = gl_ProjectionMatrix * gl_ModelViewMatrix * gl_Vertex;
-  // gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
   gl_Position = ftransform();
 }
